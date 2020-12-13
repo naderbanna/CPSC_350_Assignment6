e73beb2a853352bab09a2d78cd12d3ce64fd424e
naderbanna <naderbanna2@gmail.com>
2020-12-13 09:26:58 +0000

    minor updates

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char** argv){
   int min = 0;
   int max = 25;
   int numDouble = 10;
+
   random_device rd;
   default_random_engine eng(rd());
   uniform_real_distribution<double> distr(min, max);
@@ -23,7 +24,6 @@ int main(int argc, char** argv){
   for(int i = 0; i < numDouble; ++i){
     numFile << setprecision(4) << distr(eng) << "\n";
   }
-
   numFile.close();
 
 
@@ -31,22 +31,22 @@ int main(int argc, char** argv){
   numFile.open("randomNumbers.txt");
   string line = "";
 
-  double * values;
-  //values = new int [];
+
   getline(numFile, line);
-  values = new double[stoi(line)];
+  int arrSize = stoi(line);
 
-  //string line = "";
+  double * values;
+  values = new double[arrSize];
+
   for(int i = 0; getline(numFile, line); ++i){
-    double numV = stod(line);
-    values[i] = numV;
-
+    values[i] = stod(line);;
   }
 
 
   for (int i = 0; i < numDouble; i++){
     cout << values[i] << endl;
   }
-  cout << "end" << endl;
+
+  delete values;
   return 0;
 };
